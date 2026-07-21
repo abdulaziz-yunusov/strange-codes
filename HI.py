fd4f77cc0b83307d4ff5d893b85895ce3058a4dc
Abdulaziz Yunusov <xphp8dt74n@privaterelay.appleid.com>
2026-07-21 11:18:59 +0500

    Implement script to print 'HI' using boolean values

---
+++ HI.py
@@ -0,0 +1,6 @@
+# Valid Python 3
+# Printing word "HI" only using True
+print(
+    chr((True + True)**(True + True + True + True + True + True) + (True + True)**(True + True + True)) +
+    chr((True + True)**(True + True + True + True + True + True) + (True + True)**(True + True + True) + True)
+)
