f13da7ce7e61b5d211a92b333280f573018b6523
Abdulaziz Yunusov <xphp8dt74n@privaterelay.appleid.com>
2026-07-21 14:19:05 +0500

    Delete HI.py

--- HI.py
+++
@@ -1,6 +0,0 @@
-# Valid Python 3
-# Printing word "HI" only using True
-print(
-    chr((True + True)**(True + True + True + True + True + True) + (True + True)**(True + True + True)) +
-    chr((True + True)**(True + True + True + True + True + True) + (True + True)**(True + True + True) + True)
-)
